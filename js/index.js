6a6837e97fe4aa8d2abf66873cf488b671a9cee6
Abigail <pineverdi@gmail.com>
2022-10-18 15:32:16 -0600

    Modal: evento click con JavaScript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
-console.log("bienvenido");
-const boton1 = document.querySelector("#button1");
+const cafe_negro = document.querySelector("#cafe-negro");
+const cookies = document.querySelector("#galletas");
+const cafe_leche = document.querySelector("#cafe-leche");
+
 const modal = document.querySelector(".modal");
+
+const botonCerrar= document.querySelector(".modal__content__close");
+
+/*Eventos*/
+
+cafe_negro.addEventListener("click", ()=>{
+    modal.classList.remove("ocuta-modal");
+    modal.classList.add("mostrar-modal");
+});
+
+cookies.addEventListener("click", ()=>{
+    modal.classList.remove("ocuta-modal");
+    modal.classList.add("mostrar-modal");
+});
+
+cafe_leche.addEventListener("click", ()=>{
+    modal.classList.remove("ocuta-modal");
+    modal.classList.add("mostrar-modal");
+});
+
+botonCerrar.addEventListener("click", ()=>{
+    modal.classList.add("ocuta-modal");
+    modal.classList.remove("mostrar-modal");
+});
